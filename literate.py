2bcf7b549c3e1f4d76f33a37da079ed97d36ddf9
brupoon <albatrossian@outlook.com>
2016-05-27 17:44:26 -0400

    beginnings

---
+++ literate.py
@@ -0,0 +1,16 @@
+"""
+literate
+a text editor
+
+(c) brunston
+This program comes with absolutely zero warranty.
+"""
+
+import curses
+from curses import wrapper
+
+def main(stdscr):
+    
+    stdscr.clear()
+
+wrapper(main)
