58785c2c2e99dd43574c50b9c9c543b050612d80
Brunston Poon <albatrossian@outlook.com>
2016-05-28 11:54:32 -0400

    thinking

--- literate.py
+++ literate.py
@@ -23,14 +23,18 @@ def main(stdscr):
 def menu(stdscr):
     ch_menu = stdscr.getch()
     ch_chr = chr(ch_menu)        
+    
     if ch_menu == ord('q'):
         stdscr.addstr(ch_chr)
         sys.exit()
-
+    
     if ch_menu == ord('w'):
         stdscr.addstr(ch_chr)
-        stdscr.addstr("lol")
 
+    if ch_menu == ord('t'):
+        stdscr.addstr(ch_chr)
+        stdscr.addstr("lol")
+    
     curses.setsyx(csr_pos[0],csr_pos[1])
  
 
