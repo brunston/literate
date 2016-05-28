6894ac01260a9d9385360883e82d1abfc4354ebc
brupoon <albatrossian@outlook.com>
2016-05-28 08:37:13 -0400

    cursor goes to bottom line when called to buffer/menu, and exit works

--- literate.py
+++ literate.py
@@ -7,10 +7,31 @@ This program comes with absolutely zero warranty.
 """
 
 import curses
-from curses import wrapper
+import sys
 
 def main(stdscr):
     
     stdscr.clear()
+    while True:
+        ch = stdscr.getch()
+        csr_pos = curses.getsyx() #cursor position
+        if ch == ord('`'):
+            stdscr.addstr(curses.LINES-1, 0, chr(ch))
+            menu(stdscr)
 
-wrapper(main)
+
+def menu(stdscr):
+    ch_menu = stdscr.getch()
+    ch_chr = chr(ch_menu)        
+    if ch_menu == ord('q'):
+        stdscr.addstr(ch_chr)
+        sys.exit()
+
+    if ch_menu == ord('w'):
+        stdscr.addstr(ch_chr)
+        stdscr.addstr("lol")
+
+    curses.setsyx(csr_pos[0],csr_pos[1])
+ 
+
+curses.wrapper(main)
